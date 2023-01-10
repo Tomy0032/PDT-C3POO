f4d2536c3ce0f37ac9c1dc2aa3f915eec6b33b28
Tomy0032 <tomas.com9@gmail.com>
2023-01-10 12:44:21 -0300

    Control de versiones
    Nuevo mensaje de consola.
    Lista ITR y Generación actualizadas.

--- PDT_Cliente/src/interfaz/Main.java
+++ PDT_Cliente/src/interfaz/Main.java
@@ -12,8 +12,10 @@ public class Main {
 
 	public static void main(String[] args) throws NamingException, ServicesException{
 				
-//		Datos.cargar();		
+		Datos.cargar();		
 		Listas.cargar();
+		
+		MensajeConsola.Iniciar();
 		
 	    Registrarse r = new Registrarse();
 								    
---
+++ PDT_Cliente/src/interfaz/MensajeConsola.java
@@ -0,0 +1,17 @@
+package interfaz;
+
+public class MensajeConsola {
+
+	public static void Iniciar() {
+
+		System.out.println("");
+		System.out.println("    ####         ####   #####    ####    ####");
+		System.out.println("   #                 #  #    #  #    #  #    #");
+		System.out.println("   #       ###     ##   #####   #    #  #    #");
+		System.out.println("   #                 #  #       #    #  #    #");
+		System.out.println("    ####         ####   #        ####    ####");
+		System.out.println("");
+		System.out.println("    v0.1.2");
+	}
+
+}
